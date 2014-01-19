49d4c93675abfbd03d4cfc14ae7bc66cc47be323
Nathan Merrill <nathan8tiger@gmail.com>
2014-01-19 05:58:55 -0800

    print for debug

--- Server.py
+++ Server.py
@@ -22,6 +22,7 @@ class Server():
             data = self.getData(user)
             if (data != ""):
                 self.send(data, user)
+                print data
                 data = parse(data)
                 for d in data:
                     if d["type"] == "R":
