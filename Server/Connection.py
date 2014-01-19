5bdec2738b4106fba3e4978710a6107f22ca3987
Nathan Merrill <nathan8tiger@gmail.com>
2014-01-18 16:33:19 -0800

    Some updated server stuff - still doesn't work!

--- Server/Connection.py
+++ Server/Connection.py
@@ -4,7 +4,30 @@ import socket
 class Connection():
     """Makes connections for multiplayer"""
     def __init__(self):
-       
+        self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def recieve():
+    def pickName(self, HOST, PORT, name):
+        self.c.connect((HOST, PORT))
+        self.c.sendall(name)
+        if (self.c.recv(1) == "Y"):
+            self.c.setblocking(False)
+            return True
+        else:
+            self.c.close()
+            return False
 
+    ##################################### RECIEVE ##########################
+    def recieve(self):
+        self.recieveFromServer()
+        self.recieveFromUsers()
+
+
+    ##################################### SEND #############################
+    def send(self, message):
+        message = self.convertMessage(message)
+        try:
+            self.c.sendall(message)
+            for user in self.users:
+                self.users[user].sendall(message)
+        except socket.error:
+            pass
--- Server/Server.py
+++ Server/Server.py
@@ -4,9 +4,6 @@ import socket
 class Server():
     """Runs the Server"""
     def __init__(self, HOST, PORT):
-        self.HOST = HOST
-        self.PORT = PORT
-
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setblocking(False)
         self.s.bind((HOST, PORT))
@@ -15,13 +12,60 @@ class Server():
 
         self.users = {}
 
+    ############################# RECIEVE ##################################
     def recieve(self):
         self.getNewConnections()
-        for user in users:
+        for user in self.users:
             data = self.getData(user)
             self.parse(data)
     
     def getNewConnections(self):
         try:
             conn, addr = self.s.accept()
-            newName = conn.recv() ########### fix #########
+            newName = conn.recv(10)
+            for user in self.users:
+                if (newName == user):
+                    conn.sendall("N")
+                    conn.close()
+                    conn = "Er"
+            if (conn != "Er"):
+                conn.sendall("Y")
+                
+                conn.setblocking(False)
+
+                self.users[newName] = conn
+
+                self.sendUsers()
+        except socket.error:
+            pass
+
+    def getData(self, user):
+        try:
+            data = self.users[user].recv() ####FIX#####
+        except socket.error:
+            pass
+        return data
+    
+    ########################### SEND #######################################
+    def send(self, data):
+        try:
+            for user in self.users:
+                self.users[user].sendall(data)
+        except socket.error:
+            pass
+
+    def sendMissile(self):
+        pass
+    
+    def sendLocation(self):
+        pass
+
+    def sendExplode(self):
+        pass
+
+    def sendMake(self):
+        pass
+
+    def sendRemove(self):
+        pass
+
