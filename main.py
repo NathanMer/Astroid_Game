dd2b7c50d120dc6b288609e7bb46fa3f270a3683
rowanphipps <rowan.phipps@gmail.com>
2014-01-19 05:57:54 -0800

    sending location stuff every frame

--- main.py
+++ main.py
@@ -282,9 +282,9 @@ while True:
 
 
 	frame += 1
-	if not (frame%3):
-		x, y = player.rect.center
-		client.sendLocation(int(x),int(y), True, int(math.degrees(player.rotation)))
+	x, y = player.rect.center
+	client.sendLocation(int(x),int(y), True, int(math.degrees(player.rotation)))
+	
 
 	
 	# if rep == "y":
