0e4a661652a3a6affef9cec340555681e7f30ba4
Nathan Merrill <nathan8tiger@gmail.com>
2014-01-18 22:18:38 -0800

    This Works!!!!! Not much else thought :(

--- Server/Conversion.py
+++ Server/Conversion.py
@@ -10,13 +10,13 @@ def parse(data):
         if (command[0] == "S"):
             commands["x"] = int(command[1:5])
             commands["y"] = int(command[5:9])
-            commands["burning"] = bool(command[9])
+            commands["burning"] = bool(int(command[9]))
             commands["rotation"] = int(command[10:13])
         elif (command[0] == "L"):
             commands["name"] = command[1:11]
             commands["x"] = int(command[11:15])
             commands["y"] = int(command[15:19])
-            commands["burning"] = bool(command[19])
+            commands["burning"] = bool(int(command[19]))
             commands["rotation"] = int(command[20:23])
         elif (command[0] == "E"):
             commands["x"] = int(command[1:5])
@@ -30,13 +30,13 @@ def encode(allCommands):
     data = []
     for command in allCommands:
         if (command["type"] == "S"):
-            data.append(str(command["type"]) + str(command["x"]).zfill(5) + str(command["y"]).zfill(5) + str(int(command["burning"])) + str(command["rotation"]).zfill(3))
+            data.append(str(command["type"]) + str(command["x"]).zfill(4) + str(command["y"]).zfill(4) + str(int(command["burning"])) + str(command["rotation"]).zfill(3))
         elif (command["type"] == "L"):
-            data.append(str(command["type"]) + str(command["name"]) + str(command["x"]).zfill(5) + str(command["y"]).zfill(5) + str(int(command["burning"])) + str(command["rotation"]).zfill(3))
+            data.append(str(command["type"]) + str(command["name"]) + str(command["x"]).zfill(4) + str(command["y"]).zfill(4) + str(int(command["burning"])) + str(command["rotation"]).zfill(3))
         elif (command["type"] == "E"):
-            data.append(str(command["type"]) + str(command["x"]).zfill(5) + str(command["y"]).zfill(5))
+            data.append(str(command["type"]) + str(command["x"]).zfill(4) + str(command["y"]).zfill(4))
         else:
             data.append(str(command["type"]) + str(command["name"]))
 
-    data = data.join(",")
+    data = ",".join(data)
     return data
